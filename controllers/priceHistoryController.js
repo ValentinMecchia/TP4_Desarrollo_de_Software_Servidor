edef89e51191146ebb7182366affcb51804fa36d
ValentinMecchia <valentinmecchia@gmail.com>
2025-05-28 20:19:46 -0300

    Added new endpoints for api

--- controllers/priceHistoryController.js
+++ controllers/priceHistoryController.js
@@ -3,7 +3,7 @@ const { PriceHistory } = require('../models/PriceHistory');
 exports.getById = async (req, res) => {
     try {
         const priceHistory = await PriceHistory.findByPk(req.params.id);
-        if (priceHistory) res.json(PriceHistory);
+        if (priceHistory) res.json(priceHistory);
         else res.status(404).json({ message: 'Inversión no encontrada' });
     } catch (error) {
         res.status(500).json({ message: 'Error al obtener inversión', error });
