1482ccd87791c91f9eeaded6a0a144b435381598
ValentinMecchia <valentinmecchia@gmail.com>
2025-06-18 20:20:20 -0300

    modificado index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === 'production' ? true : false,
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 24 * 60 * 60 * 1000,
   }
