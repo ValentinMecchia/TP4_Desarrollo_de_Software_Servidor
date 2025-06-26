37e0cc56810798a25b37f6d342b01f0a376e426f
ValentinMecchia <valentinmecchia@gmail.com>
2025-06-26 14:56:38 -0300

    Migrando base de datos por expiracion

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: 'postgres',
-        port: process.env.DB_PORT,
+        port: Number(process.env.DB_PORT),
         logging: false,
         dialectOptions: {
             ssl: {
